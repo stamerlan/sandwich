ab1ba1270e37ba8e8217bed223139e7c11b04d77
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-05-14 17:16:07 +0900

    Add .editorconfig

--- src/retarget.cc
+++ src/retarget.cc
@@ -27,37 +27,37 @@ namespace uart {
  */
 static void init(void)
 {
-    unsigned int r;
+	unsigned int r;
 
-    /* initialize UART */
-    *AUX_ENABLE = *AUX_ENABLE | 1;       // enable UART1, AUX mini uart
-    *AUX_MU_CNTL = 0;
-    *AUX_MU_LCR = 3;       // 8 bits
-    *AUX_MU_MCR = 0;
-    *AUX_MU_IER = 0;
-    *AUX_MU_IIR = 0xc6;    // disable interrupts
-    *AUX_MU_BAUD = 270;    // 115200 baud
-    /* map UART1 to GPIO pins */
-    r=*GPFSEL1;
-    r&=~((7<<12)|(7<<15)); // gpio14, gpio15
-    r|=(2<<12)|(2<<15);    // alt5
-    *GPFSEL1 = r;
-    *GPPUD = 0;            // enable pins 14 and 15
-    r=150; while(r--) { asm volatile("nop"); }
-    *GPPUDCLK0 = (1<<14)|(1<<15);
-    r=150; while(r--) { asm volatile("nop"); }
-    *GPPUDCLK0 = 0;        // flush GPIO setup
-    *AUX_MU_CNTL = 3;      // enable Tx, Rx
+	/* initialize UART */
+	*AUX_ENABLE = *AUX_ENABLE | 1;       // enable UART1, AUX mini uart
+	*AUX_MU_CNTL = 0;
+	*AUX_MU_LCR = 3;       // 8 bits
+	*AUX_MU_MCR = 0;
+	*AUX_MU_IER = 0;
+	*AUX_MU_IIR = 0xc6;    // disable interrupts
+	*AUX_MU_BAUD = 270;    // 115200 baud
+	/* map UART1 to GPIO pins */
+	r=*GPFSEL1;
+	r&=~((7<<12)|(7<<15)); // gpio14, gpio15
+	r|=(2<<12)|(2<<15);    // alt5
+	*GPFSEL1 = r;
+	*GPPUD = 0;            // enable pins 14 and 15
+	r=150; while(r--) { asm volatile("nop"); }
+	*GPPUDCLK0 = (1<<14)|(1<<15);
+	r=150; while(r--) { asm volatile("nop"); }
+	*GPPUDCLK0 = 0;        // flush GPIO setup
+	*AUX_MU_CNTL = 3;      // enable Tx, Rx
 }
 
 /**
  * Send a character
  */
 void putchar(int c) {
-    /* wait until we can send */
-    do{asm volatile("nop");}while(!(*AUX_MU_LSR&0x20));
-    /* write the character to the buffer */
-    *AUX_MU_IO=c;
+	/* wait until we can send */
+	do{asm volatile("nop");}while(!(*AUX_MU_LSR&0x20));
+	/* write the character to the buffer */
+	*AUX_MU_IO=c;
 }
 
 } /* namespace uart */
