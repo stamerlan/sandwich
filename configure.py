c7c573deab153b31d454b4b58ee63229c9af95bb
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-05-16 16:30:35 +0900

    Load raspi3 platform from configure.py

--- configure.py
+++ configure.py
@@ -1,59 +1,14 @@
 #!/usr/bin/env python3
-import os
-import pathlib
 import scripts.fwbuild as fwbuild
-import sys
 
-target_platform = "raspi3"
-#target_platform = "win"
+fwbuild.platform.load(fwbuild.srcdir / "platform/raspi3b/platform.py")
 
-hello = fwbuild.cxx_target("hello")
-hello.cxxflags += "-std=c++23", "-g", "-O3"
-hello.cxxflags += "-fcheck-new", "-flto", "-fno-rtti", "-fno-exceptions"
-hello.cxxflags += "-fno-threadsafe-statics"
-hello.cxxflags += "-Wall", "-Wextra", "-Werror", "-Weffc++",
-hello.cxxflags += "-Wmultiple-inheritance", "-Wvirtual-inheritance"
-hello.cxxflags += "-ffile-prefix-map=$srcdir/="
+fw = fwbuild.platform.cxx_target("hello")
+fw.cxxflags += "-std=c++23", "-g", "-O3"
+fw.cxxflags += "-fcheck-new", "-flto", "-fno-rtti", "-fno-exceptions"
+fw.cxxflags += "-fno-threadsafe-statics"
+fw.cxxflags += "-Wall", "-Wextra", "-Werror", "-Weffc++",
+fw.cxxflags += "-Wmultiple-inheritance", "-Wvirtual-inheritance"
+fw.cxxflags += "-ffile-prefix-map=$srcdir/="
 
-hello.src("src/main.cc")
-
-if target_platform == "win":
-    gcc = fwbuild.gcc.find()
-elif target_platform == "raspi3":
-    gcc = fwbuild.gcc.find("aarch64-none-elf-")
-
-    hello.gen_binary = True
-    hello.gen_dasm = True
-    hello.gen_map = True
-
-    hello.cxxflags += "-march=armv8-a+crc", "-mcpu=cortex-a53"
-
-    hello.ldflags += "-nostartfiles", "-specs=nosys.specs"
-    hello.ldflags += "-flto"
-
-    print(f"Binutils v{'.'.join(str(v) for v in gcc.ld.version)}")
-    if gcc.ld.version >= (2, 39):
-        hello.ldflags += "-Wl,--no-warn-rwx-segment"
-
-    hello.ldscript = "src/raspi3b.ld"
-
-    hello.ldlibs += "-lgcc"
-
-    hello.src("src/raspi3_startup.S")
-    hello.src("src/raspi3_init.cc")
-    hello.src("src/retarget.cc", variables={"cxxflags": "$cxxflags -fno-lto"})
-else:
-    print(f"Unknown platform '{target_platform}'", file=sys.stderr)
-    sys.exit(1)
-
-fwbuild.outdir.mkdir(parents=True, exist_ok=True)
-with open(fwbuild.outdir / "build.ninja", "w") as buildfile:
-    gcc.write_ninja_file(buildfile, hello)
-    n = fwbuild.NinjaWriter(buildfile)
-    n.newline()
-    n.comment("Regenerate build file if build script changed.")
-    n.rule("configure",
-        command=f"{pathlib.Path(sys.executable).as_posix()} {pathlib.Path(__file__).as_posix()}",
-        generator=True,
-        description="CONFIGURE")
-    n.build("$outdir/build.ninja", "configure", implicit=hello.regen_on)
+fw.src("src/main.cc")
