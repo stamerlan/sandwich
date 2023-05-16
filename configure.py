1dd297e0c5f8420566f5778044b21189829fdc30
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-05-16 19:33:29 +0900

    Add --platform cmdline argument to configure.py

--- configure.py
+++ configure.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+import argparse
 import scripts.fwbuild as fwbuild
 
-fwbuild.platform.load(fwbuild.srcdir / "platform/raspi3b/platform.py")
-#fwbuild.platform.load(fwbuild.srcdir / "platform/host/platform.py")
+parser = argparse.ArgumentParser("Sandwich configuration script")
+parser.add_argument("--platform", choices=["raspi3b", "host"],
+                    default="raspi3b", help="Target platform")
+conf = parser.parse_args()
+
+fwbuild.platform.load(fwbuild.srcdir / f"platform/{conf.platform}/platform.py")
+if str(fwbuild.outdir) != ".":
+    fwbuild.outdir /= conf.platform
 
 fw = fwbuild.platform.cxx_target("fw")
 fw.cxxflags += "-std=c++23", "-g", "-O3"
