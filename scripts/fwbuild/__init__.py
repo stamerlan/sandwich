90c7d216b4173e5916de2d749e29ac8d65cca477
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-05-16 16:22:35 +0900

    Make fwbuild package available as "fwbuild"

--- scripts/fwbuild/__init__.py
+++ scripts/fwbuild/__init__.py
@@ -3,6 +3,11 @@ import sys
 from .cxx_target import cxx_target
 from .toolchains.gcc import gcc
 
+# Make the package available by name 'fwbuild'
+if __name__ != "fwbuild":
+    sys.modules["fwbuild"] = sys.modules[__name__]
+
+# Setup default srcdir and outdir
 srcdir = pathlib.Path(sys.modules["__main__"].__file__).parent
 if srcdir.samefile(pathlib.Path.cwd()):
     outdir = pathlib.Path("bin/")
