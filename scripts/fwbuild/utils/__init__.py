836af7eb5420abd87b4eb4a47b863339279a20b1
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-05-17 23:30:21 +0900

    Change fwbuild.utils.ninja_syntax module import to get IDE syntax highlighting

--- scripts/fwbuild/utils/__init__.py
+++ scripts/fwbuild/utils/__init__.py
@@ -3,4 +3,4 @@ from .ninja_writer import ninja_writer
 from .program import program
 from .src_path import src_path
 from .str_list import str_list
-import fwbuild.utils.ninja_syntax
+from . import ninja_syntax
