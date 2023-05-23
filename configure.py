370b49bfd5c3a24ece86b20654ff309c855cf310
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-05-23 10:45:08 +0900

    Add build.py for sources

--- configure.py
+++ configure.py
@@ -11,17 +11,5 @@ fwbuild.platform.load(f"platform/{conf.platform}/platform.py")
 if str(fwbuild.topout) != ".":
     fwbuild.topout /= conf.platform
 
-app = fwbuild.platform.cxx_target("hello")
-app.gen_dasm = True
-app.gen_map = True
+import src.build
 
-app.cxxflags += "-std=c++23", "-g", "-O3"
-app.cxxflags += "-fcheck-new", "-flto", "-fno-rtti", "-fno-exceptions"
-app.cxxflags += "-fno-threadsafe-statics"
-app.cxxflags += "-Wall", "-Wextra", "-Werror", "-Weffc++",
-app.cxxflags += "-Wmultiple-inheritance", "-Wvirtual-inheritance"
-app.cxxflags += "-ffile-prefix-map=$srcdir/="
-
-app.ldflags += "-flto"
-
-app.src("src/main.cc")
---
+++ src/build.py
@@ -0,0 +1,16 @@
+import fwbuild
+
+app = fwbuild.platform.cxx_target("hello")
+app.gen_dasm = True
+app.gen_map = True
+
+app.cxxflags += "-std=c++23", "-g", "-O3"
+app.cxxflags += "-fcheck-new", "-flto", "-fno-rtti", "-fno-exceptions"
+app.cxxflags += "-fno-threadsafe-statics"
+app.cxxflags += "-Wall", "-Wextra", "-Werror", "-Weffc++"
+app.cxxflags += "-Wmultiple-inheritance", "-Wvirtual-inheritance"
+app.cxxflags += "-ffile-prefix-map=$srcdir/="
+
+app.ldflags += "-flto"
+
+app.src("main.cc")
