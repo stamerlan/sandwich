0bb020248908e15260f064c1b4d5711f84469ff5
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-05-24 19:28:24 +0900

    Add Kconfig support

--- configure.py
+++ configure.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 import argparse
+import kconfiglib
+import menuconfig
+import os
 import scripts.fwbuild as fwbuild
 
 parser = argparse.ArgumentParser(description="Sandwich configuration script")
-parser.add_argument("--platform", choices=["raspi3b", "host"],
-                    default="host", help="Target platform")
-conf = parser.parse_args()
+parser.add_argument("-c", "--config", "--cfg", "--conf",
+    help="Filename to load configuration from")
+args = parser.parse_args()
 
-fwbuild.platform.load(f"platform/{conf.platform}/{conf.platform}.py")
+os.environ["srctree"] = str(fwbuild.topdir)
+kconf = kconfiglib.Kconfig(fwbuild.topdir / "Kconfig")
+if args.config is None:
+    args.config = ".config"
+    menuconfig.menuconfig(kconf)
+else:
+    kconf.load_config(args.config)
+
+if kconf.syms["PLATFORM_HOST"].tri_value:
+    platform_name = "host"
+elif kconf.syms["PLATFORM_RASPI3B"].tri_value:
+    platform_name = "raspi3b"
+
+fwbuild.platform.load(f"platform/{platform_name}/{platform_name}.py")
 if str(fwbuild.topout) != ".":
-    fwbuild.topout /= conf.platform
+    fwbuild.topout /= platform_name
 
 import src.build
