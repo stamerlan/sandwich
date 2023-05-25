da4468cbabb697d0d49395b8192a42655c7a6d84
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-05-25 18:37:17 +0900

    Add empty functions if uart is disabled

--- drivers/uart/include/uart.h
+++ drivers/uart/include/uart.h
@@ -1,10 +1,17 @@
 #ifndef DRIVERS_UART_H
 #define DRIVERS_UART_H
 
+#include <config.h>
+
 namespace uart {
 
+#ifdef CONFIG_UART
 void init(void);
 int putchar(int c);
+#else
+static inline void init(void) {}
+static inline int putchar(int c) { return (unsigned char)c; }
+#endif
 
 } /* namespace uart */
 
