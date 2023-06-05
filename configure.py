7144a8b11626661d86b2b92f2a0dccc0d13c58d8
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-05 23:27:23 +0900

    Use fwbuild.include() instead of import

--- configure.py
+++ configure.py
@@ -15,16 +15,14 @@ print(fwbuild.conf.load_kconfig(args.config))
 
 # Load platform-specific code
 if fwbuild.conf.PLATFORM_HOST:
-    import platforms.host
-    fwbuild.platform = platforms.host.HostPlatform()
+    fwbuild.platform = fwbuild.include("platforms/host.py").HostPlatform()
 elif fwbuild.conf.PLATFORM_RASPI3B:
-    import platforms.raspi3b
-    fwbuild.platform = platforms.raspi3b.Raspi3bPlatform()
+    fwbuild.platform = fwbuild.include("platforms/raspi3b").Raspi3bPlatform()
 
 # Setup output directory
 fwbuild.set_topout("bin", fwbuild.platform.name)
 
 # Add sources
-import src.build
+fwbuild.include("src/build.py")
 
 fwbuild.platform.write_buildfiles(__file__)
