176606ca3e46ea64e0f5b65591362e714450b9cd
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-07 10:57:00 +0900

    Don't use PATHEXT on win32 if program name already has an extension

--- scripts/fwbuild/utils/program.py
+++ scripts/fwbuild/utils/program.py
@@ -8,15 +8,15 @@ class program(object):
     def __init__(self, *path_parts):
         fname = pathlib.Path(*path_parts)
 
-        if sys.platform == "win32":
+        if sys.platform != "win32" or fname.suffix:
+            files = [fname]
+        else:
             # CMD defaults in Windows 10
             WIN_DEFAULT_PATHEXT = ".COM;.EXE;.BAT;.CMD;.VBS;.JS;.WS;.MSC"
             ext = os.getenv("PATHEXT") or WIN_DEFAULT_PATHEXT
             ext = [e.lower() for e in ext.split(os.pathsep) if e]
 
-            files = [fname] + [fname.parent / (fname.name + e) for e in ext]
-        else:
-            files = [fname]
+            files = [fname] + [fname.with_suffix(e) for e in ext]
 
         for f in files:
             if f.is_file() and os.access(f, os.X_OK):
