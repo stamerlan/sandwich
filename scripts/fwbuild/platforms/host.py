1a1f10b195cc67890f6a681d41ef4fec58ade9da
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-07 16:01:18 +0900

    Host platform: use base platform implementation

--- scripts/fwbuild/platforms/host.py
+++ scripts/fwbuild/platforms/host.py
@@ -1,19 +1,9 @@
 import fwbuild
 import fwbuild.platforms
 import fwbuild.targets
-import fwbuild.toolchains
 import fwbuild.utils
 
 fwbuild.deps.add(__file__)
 
 class host(fwbuild.platforms.base):
     name: str = "host"
-    toolchain: fwbuild.toolchains.gcc | None = None
-
-    def __init__(self):
-        if host.toolchain is None:
-            host.toolchain = fwbuild.toolchains.gcc.find()
-
-    def cxx_app(self, name: str) -> fwbuild.targets.cxx_app:
-        srcdir = fwbuild.utils.caller().dir
-        return super().cxx_app(name, host.toolchain, srcdir)
