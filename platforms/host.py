04409a17f5aa4d3b004396548a4426df9daeb480
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-13 21:45:50 +0900

    Add host platform

---
+++ platforms/host.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import fwbuild
+import fwbuild.toolchains
+
+class host(object):
+    def __init__(self, conf: fwbuild.kconfig):
+        fwbuild.deps.add(Path(__file__))
+
+        self.targets = []
+        self.toolchain = fwbuild.toolchains.gcc.find()
+
+        for cls in fwbuild.targets:
+            if issubclass(cls, fwbuild.cxx_gtest):
+                print(f"{cls.__name__}: GTest target is not supported")
+            elif issubclass(cls, fwbuild.cxx_app):
+                self.targets.append(cls(conf, self.toolchain))
+            elif issubclass(cls, fwbuild.cxx_module):
+                pass
+            else:
+                raise RuntimeError(f"Unexpected target {cls}")
--- platforms/host/host.py
+++
@@ -1,7 +0,0 @@
-import fwbuild
-import fwbuild.platforms
-import fwbuild.targets
-import fwbuild.utils
-
-class host(fwbuild.platforms.base):
-    name: str = "host"
