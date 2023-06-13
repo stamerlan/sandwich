4ff13a35d500bd2e07da79651a8d255f596bf15a
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-13 20:42:43 +0900

    Remove file_set class

--- scripts/fwbuild/__init__.py
+++ scripts/fwbuild/__init__.py
@@ -1,14 +1,13 @@
 from .cxx_app import cxx_app
 from .cxx_gtest import cxx_gtest
 from .cxx_module import cxx_module
-from .file_set import file_set
 from .kconfig import kconfig
 from .target import target, targets
 from .tool import tool
 from .toolchain import toolchain
+from pathlib import Path
 import contextlib
 import os
-import pathlib
 import sys
 
 # Make the package available by name 'fwbuild'
@@ -16,13 +15,13 @@ if __name__ != "fwbuild":
     sys.modules["fwbuild"] = sys.modules[__name__]
 
 # Top source directory. The directory where configuration script located
-topdir = pathlib.Path(sys.modules["__main__"].__file__).parent
+topdir = Path(sys.modules["__main__"].__file__).parent
 
 # Configuration script dependencies. Changes in those files trigger invoking
 # configuration script before the build.
-deps = file_set(topdir, sys.modules["__main__"].__file__)
-for root, dirs, files in os.walk(pathlib.Path(__file__).parent):
+deps: set[Path] = set([Path(sys.modules["__main__"].__file__)])
+for root, dirs, files in os.walk(Path(__file__).parent):
     for file in files:
-        deps.add(pathlib.Path(root, file))
+        deps.add(Path(root, file))
     with contextlib.suppress(ValueError):
         dirs.remove("__pycache__")
