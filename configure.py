88de14d074abc7ef23e9b72833d8f700df322cfe
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-13 16:16:36 +0900

    Remove cxx_module dependency from cxx_app module

--- configure.py
+++ configure.py
@@ -12,10 +12,28 @@ args = parser.parse_args()
 conf = fwbuild.kconfig(fwbuild.topdir)
 print(conf.load_config(args.config))
 
-if conf.PLATFORM_HOST:
-    build = fwbuild.ninja(PlatformHost(conf), "bin/host/ninja.build")
-elif conf.PLATFORM_RASPI3B:
-    build = fwbuild.ninja(PlatformRaspi3b(conf), "bin/raspi3b/ninja.build")
-else:
-    raise RuntimeError("Unknown platform")
-fwbuild.vscode(build, ".vscode/")
+#class hello(fwbuild.cxx_app):
+#    def __init__(self, conf, toolchain):
+#        super().__init__(conf, toolchain)
+#m = hello(None, None)
+m = fwbuild.cxx_app(None, None)
+print(m)
+m.cxxflags += '-Wall', "-Wextra"
+print(f"  cxxflags: {m.cxxflags}")
+m.src("src/main.cc", cxxflags="-Os")
+for s in m.sources:
+    print(f"{s.path.as_posix()} {s.meta}")
+
+#@fwbuild.target
+#class hello(fwbuild.cxx_app):
+#    def init(self, conf, toolchain):
+#        super().init(conf, toolchain)
+#        self.src("main.cc")
+
+#if conf.PLATFORM_HOST:
+#    build = fwbuild.ninja(PlatformHost(conf), "bin/host/ninja.build")
+#elif conf.PLATFORM_RASPI3B:
+#    build = fwbuild.ninja(PlatformRaspi3b(conf), "bin/raspi3b/ninja.build")
+#else:
+#    raise RuntimeError("Unknown platform")
+#fwbuild.vscode(build, ".vscode/")
--- scripts/fwbuild/cxx_app.py
+++ scripts/fwbuild/cxx_app.py
@@ -1,5 +1,11 @@
-class cxx_app(object):
+from .caller import caller
+from .cxx_module import cxx_module
+
+class cxx_app(cxx_module):
     """ Base class for C++ application targets """
 
-    def __init__(self, conf, toolchain):
-        pass
+    def __init__(self, conf, toolchain, name: str | None = None):
+        if name is None:
+            name = self.__class__.__name__
+        print("cxx_app:", name)
+        super().__init__(self, name, caller().dir)
