ec55aa13bb834bbad7f2aead6dfa30032d68c4ae
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-13 10:30:32 +0900

    Add fwbuild.topdir

--- scripts/fwbuild/__init__.py
+++ scripts/fwbuild/__init__.py
@@ -1,8 +1,12 @@
 from .cxx_app import cxx_app
 from .kconfig import kconfig
 from .target import target
+import pathlib
 import sys
 
 # Make the package available by name 'fwbuild'
 if __name__ != "fwbuild":
     sys.modules["fwbuild"] = sys.modules[__name__]
+
+# Top source directory. The directory where configuration script located
+topdir = pathlib.Path(sys.modules["__main__"].__file__).parent
