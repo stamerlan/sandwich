57798458c82c75a7cab76f404c7e3c044c0d4dee
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-13 19:04:44 +0900

    Add test code to configure.py

--- configure.py
+++ configure.py
@@ -12,17 +12,75 @@ args = parser.parse_args()
 conf = fwbuild.kconfig(fwbuild.topdir)
 print(conf.load_config(args.config))
 
-#class hello(fwbuild.cxx_app):
-#    def __init__(self, conf, toolchain):
-#        super().__init__(conf, toolchain)
-#m = hello(None, None)
-m = fwbuild.cxx_app(None, None)
-print(m)
-m.cxxflags += '-Wall', "-Wextra"
-print(f"  cxxflags: {m.cxxflags}")
-m.src("src/main.cc", cxxflags="-Os")
-for s in m.sources:
-    print(f"{s.path.as_posix()} {s.meta}")
+def print_cxx_app(app: fwbuild.cxx_app):
+    print(app)
+    print(f"  cxxflags: {app.cxxflags}")
+    print(f"  includes: {[str(i) for i in app.includes]}")
+    for s in app.sources:
+        print(f"  {s.path.as_posix()}", end='')
+        if s.meta:
+            print(f" {s.meta}", end='')
+        print()
+
+def print_toolchain(toolchain: fwbuild.toolchain):
+    print(f"{toolchain.name}@{toolchain.tools.cc.path.parent.as_posix()}")
+    for name, tool in toolchain.tools.items():
+        print(f"  {name}: {tool}")
+
+@fwbuild.target
+class hello(fwbuild.cxx_app):
+    def __init__(self, conf, toolchain):
+        super().__init__(conf, toolchain)
+
+        self.cxxflags += "-Wall", "-Wextra"
+        self.include("drivers/uart/include")
+
+        self.src("src/main.cc", cxxflags="-Os")
+        self.src("drivers/uart/src/host.cc")
+app = hello(None, None)
+
+class gcc(fwbuild.toolchain):
+    def __init__(self, prefix: str = "", *search_dirs):
+        super().__init__(prefix + "gcc")
+        self._prefix = prefix
+
+        for cc in fwbuild.tool.find_all("gcc", search_dirs, name="cc"):
+            try:
+                self.tools.cc = cc
+
+                dir = cc.path.parent
+                tools = {
+                    "ar":  prefix + "ar",
+                    "cxx": prefix + "g++",
+                    "ld":  prefix + "ld",
+                    "objcopy": prefix + "objcopy",
+                    "objdump": prefix + "objdump",
+                }
+
+                for name, prog in tools.items():
+                    self.tools[name] = fwbuild.tool(dir, prog, name=name)
+                break
+            except FileNotFoundError:
+                continue
+        else:
+            raise FileNotFoundError(f'Toolchain "{prefix}gcc" not found')
+
+
+class host_platform(object):
+    def __init__(self, conf: fwbuild.kconfig):
+        self._toolchain = gcc()
+        self._targets = []
+        for cls in fwbuild.targets:
+            if issubclass(cls, fwbuild.cxx_app):
+                self._targets.append(cls(conf, self._toolchain))
+            else:
+                raise RuntimeError(f"Unexpected target {cls}")
+
+        print_toolchain(self._toolchain)
+        for target in self._targets:
+            print_cxx_app(target)
+
+host = host_platform(conf)
 
 #@fwbuild.target
 #class hello(fwbuild.cxx_app):
