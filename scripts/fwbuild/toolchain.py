6d04acf769f3c0c93d90c3503ccd99ce835f6c7a
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-17 13:44:31 +0900

    Use argparse namespace instead of tools_ns

--- scripts/fwbuild/toolchain.py
+++ scripts/fwbuild/toolchain.py
@@ -1,46 +1,9 @@
-from typing import Any, ItemsView
 from .tool import tool
-
-class tools_ns(object):
-    def __init__(self):
-        self._tools: dict[str, tool] = {}
-
-    def __getitem__(self, name: str) -> tool:
-        return self._tools[name]
-
-    def __setitem__(self, name: str, value: Any):
-        if not isinstance(value, tool):
-            value = tool(value, name=name)
-
-        self._tools[name] = value
-        self.__dict__[name] = value
-
-    def __delitem__(self, name: str):
-        del self._tools[name]
-        del self.__dict__[name]
-
-    def __setattr__(self, name: str, value: Any):
-        if name == "_tools":
-            self.__dict__["_tools"] = value
-            return
-
-        if not isinstance(value, tool):
-            value = tool(value)
-
-        self._tools[name] = value
-        self.__dict__[name] = value
-
-    def __delattr__(self, name: str):
-        del self._tools[name]
-        del self.__dict__[name]
-
-    def items(self) -> ItemsView[str, tool]:
-        return self._tools.items()
-
+from argparse import Namespace
 
 class toolchain(object):
     """ Base class for toolchain implementation """
 
     def __init__(self, name: str):
         self.name = name
-        self.tools = tools_ns()
+        self.tools = Namespace()
