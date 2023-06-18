e58bb6b0342dd116bfa05b630a988c5bc91c2988
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-18 14:55:46 +0900

    Add platform name

--- scripts/fwbuild/platform_base.py
+++ scripts/fwbuild/platform_base.py
@@ -5,6 +5,7 @@ class platform_base(object):
     def __init__(self, conf: "fwbuild.kconfig"):
         fwbuild.deps.add(Path(__file__))
         fwbuild.deps.add(fwbuild.caller().filename)
+        self.name = fwbuild.caller().cls.__name__
 
     def build_cxx_app(self, topout: Path, target: "fwbuild.cxx_app",
                       w: "fwbuild.ninja_writer"):
