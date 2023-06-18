ff969725812a47d6e3ba4c4d39725eb86f13571a
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-18 13:02:38 +0900

    add src/build.py toolchain type annotation

--- src/build.py
+++ src/build.py
@@ -3,7 +3,7 @@ import fwbuild
 # TODO: Flags are defined for GCC toolchain only
 @fwbuild.build
 class hello(fwbuild.cxx_app):
-    def __init__(self, conf: fwbuild.kconfig, toolchain):
+    def __init__(self, conf: fwbuild.kconfig, toolchain: fwbuild.toolchain):
         super().__init__(conf, toolchain)
 
         self.disassembly = True
