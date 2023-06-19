f9d71ab55f03551ba69d8d55de4f61315d845d6e
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-19 13:57:06 +0900

    Allow platform define vscode launch task

--- scripts/fwbuild/platforms/host.py
+++ scripts/fwbuild/platforms/host.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import contextlib
 import fwbuild
 import fwbuild.toolchains
 
@@ -20,3 +21,28 @@ class host(fwbuild.platform_base):
     def build_cxx_app(self, topout: Path, target: fwbuild.cxx_app,
                       w: fwbuild.ninja_writer) -> fwbuild.cxx_app.artifacts:
         return target.toolchain.build_cxx_app(topout, target, w)
+
+    def vscode_launch(self, topout: Path, target: "fwbuild.cxx_app",
+            artifacts: "fwbuild.cxx_app.artifacts") -> \
+                dict[str, str] | None:
+
+        launch_conf = {
+            "request": "launch",
+            "externalConsole": False,
+            "args": [],
+            "stopAtEntry": False,
+            "cwd": "${workspaceFolder}",
+            "environment": []
+        }
+        program = topout / artifacts.app
+        launch_conf["program"] = program
+
+        with contextlib.suppress(FileNotFoundError):
+            if isinstance(target.toolchain, fwbuild.toolchains.gcc):
+                gdb = fwbuild.tool(target.toolchain.tools.cc.path.parent,
+                                   target.toolchain.prefix + "gdb")
+                launch_conf["type"] = "cppdbg"
+                launch_conf["MIMode"] = "gdb"
+                launch_conf["miDebuggerPath"] = str(gdb)
+
+        return launch_conf
