cfb709a486c6d4057a8df5f62b63364fb32bc66f
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-19 12:40:05 +0900

    Move writing launch.json to function

--- scripts/fwbuild/vscode.py
+++ scripts/fwbuild/vscode.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import contextlib
 import fwbuild
 import fwbuild.toolchains
 import json
@@ -25,9 +26,6 @@ def merge_conf(conf_list: list[dict[str, str]], new_conf: dict[str, str],
             if conf.get(k, None) != v:
                 break
         else:
-            print("Update", conf)
-            print("New value:", conf | new_conf)
-
             found = True
             new_conf_list.append(conf | new_conf)
             continue
@@ -38,6 +36,78 @@ def merge_conf(conf_list: list[dict[str, str]], new_conf: dict[str, str],
         new_conf_list.append(new_conf)
 
     return new_conf_list
+
+
+def mk_launch_conf(topout: Path, workspace: Path,
+        platform: "fwbuild.platform_base", target: "fwbuild.cxx_app",
+        artifacts: "fwbuild.cxx_app.artifacts") -> dict[str, str]:
+    if isinstance(target, fwbuild.cxx_gtest):
+        conf_name = f"{platform.name}: test {target.name}"
+    else:
+        conf_name = f"{platform.name}: {target.name}"
+
+    launch_conf = {
+        "name": conf_name,
+        "type":"cppdbg",
+        "request": "launch",
+        "externalConsole": False,
+        "args": [],
+        "stopAtEntry": False,
+        "cwd": "${workspaceFolder}",
+        "environment": []
+    }
+    program = topout / artifacts.app
+    if program.is_relative_to(workspace):
+        program = Path("${workspaceFolder}", program.relative_to(workspace))
+    launch_conf["program"] = program.as_posix()
+
+    with contextlib.suppress(FileNotFoundError):
+        if isinstance(target.toolchain, fwbuild.toolchains.gcc):
+            gdb = fwbuild.tool(target.toolchain.tools.cc.path.parent,
+                               target.toolchain.prefix + "gdb")
+            launch_conf["MIMode"] = "gdb"
+            launch_conf["miDebuggerPath"] = str(gdb)
+
+    return launch_conf
+
+
+def write_launch_json(filename: Path, topout: Path, workspace: Path,
+        platform: "fwbuild.platform_base",
+        artifacts: dict["fwbuild.cxx_app", "fwbuild.cxx_app.artifacts"]):
+    launch = {}
+    if filename.is_file():
+        with open(filename, "r") as f:
+            launch = json.load(f, cls=JSONWithCommentsDecoder)
+
+    if "version" not in launch:
+        launch["version"] = "0.2.0"
+    if "configurations" not in launch:
+        launch["configurations"] = []
+
+    # Add applications to launch.json
+    for target, build in artifacts.items():
+        if isinstance(target, fwbuild.cxx_gtest):
+            continue
+        if not isinstance(target, fwbuild.cxx_app):
+            raise RuntimeError(f"Unexpected target type {type(target)}")
+
+        launch_conf = mk_launch_conf(topout, workspace, platform, target, build)
+        launch["configurations"] = merge_conf(launch["configurations"],
+            launch_conf, name=launch_conf["name"])
+
+    # Add tests to launch.json
+    for target, build in artifacts.items():
+        if not isinstance(target, fwbuild.cxx_gtest):
+            continue
+
+        launch_conf = mk_launch_conf(topout, workspace, platform, target, build)
+        launch["configurations"] = merge_conf(launch["configurations"],
+            launch_conf, name=launch_conf["name"])
+
+    # Write updated file
+    filename.parent.mkdir(parents=True, exist_ok=True)
+    with open(filename, "w") as f:
+        json.dump(launch, f, indent=4)
 
 
 def vscode(platform: "fwbuild.platform_base",
@@ -53,52 +123,5 @@ def vscode(platform: "fwbuild.platform_base",
 
     workspace_folder = vscode_dir.parent
 
-    launch = {}
-    launch_json = vscode_dir / "launch.json"
-    if launch_json.is_file():
-        with open(launch_json, "r") as f:
-            launch = json.load(f, cls=JSONWithCommentsDecoder)
-
-    if "version" not in launch:
-        launch["version"] = "0.2.0"
-    if "configurations" not in launch:
-        launch["configurations"] = []
-
-    for target, build in artifacts.items():
-        if isinstance(target, fwbuild.cxx_gtest):
-            conf_name = f"{platform.name}: test {target.name}"
-        else:
-            conf_name = f"{platform.name}: {target.name}"
-
-        launch_conf = {
-            "name": conf_name,
-            "type":"cppdbg",
-            "request": "launch",
-            "externalConsole": False,
-            "args": [],
-            "stopAtEntry": False,
-            "cwd": "${workspaceFolder}",
-            "environment": []
-        }
-
-        program = topout / build.app
-        if program.is_relative_to(workspace_folder):
-            program = Path("${workspaceFolder}", program.relative_to(workspace_folder))
-        launch_conf["program"] = program.as_posix()
-
-        if isinstance(target.toolchain, fwbuild.toolchains.gcc):
-            try:
-                gdb = fwbuild.tool(target.toolchain.tools.cc.path.parent,
-                                   target.toolchain.prefix + "gdb")
-
-                launch_conf["MIMode"] = "gdb"
-                launch_conf["miDebuggerPath"] = str(gdb)
-            except FileNotFoundError:
-                pass
-
-        launch["configurations"] = merge_conf(launch["configurations"],
-            launch_conf, name=conf_name)
-
-    launch_json.parent.mkdir(parents=True, exist_ok=True)
-    with open(launch_json, "w") as f:
-        json.dump(launch, f, indent=4)
+    write_launch_json(vscode_dir / "launch.json", topout, workspace_folder,
+                      platform, artifacts)
