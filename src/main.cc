71f29890a8496ec1f1e5125e5b97fdb2fe89b42e
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-28 14:38:02 +0900

    Add dummy tasks to main

--- src/main.cc
+++ src/main.cc
@@ -2,10 +2,30 @@
 #include <uart.h>
 #include <sandwich/sched.h>
 
+static void sched_task(void)
+{
+	printf("task\n");
+}
+
+static void sched_task2(void)
+{
+	printf("task2\n");
+}
+
 int main(void)
 {
 	uart::init();
 	sandwich::sched::init();
+
+	sandwich::sched::task_t task("task", sched_task);
+	task.wakeup();
+
+	sandwich::sched::task_t task2("task2", sched_task2);
+	task2.wakeup();
+
+	for (int i = 0; i < 8; i++)
+		sandwich::sched::run();
+
 
 	return 0;
 }
