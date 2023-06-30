9a8a95b5b8d8cd7e36b549b52d75312dc22464d5
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-06-30 23:38:32 +0900

    move mailbox isr

--- src/main.cc
+++ src/main.cc
@@ -1,25 +1,16 @@
 #include <cerrno>
+#include <cstdint>
 #include <cstdio>
+#include <arch/irq.h>
 #include <bcm2837_mbox.h>
 #include <sandwich/sched.h>
 #include <uart.h>
-
-#include <arch/irq.h>
-#include <cstdint>
-static volatile uint32_t* const arm_base_irq_pending = (uint32_t *)0x3F00'B200;
-static volatile uint32_t* const arm_base_irq_en = (uint32_t *)0x3F00'B218;
 
 static unsigned get_current_el(void)
 {
 	unsigned current_el;
 	asm volatile("mrs	%0, CurrentEL\n\t" : "=r"(current_el));
 	return (current_el & 0xC) >> 2;
-}
-
-extern "C" char vectors[];
-static void set_vbar_el1(void *vectors)
-{
-	asm volatile("msr	vbar_el1, %0\n\t" :: "r"(vectors));
 }
 
 static void sched_task(void)
@@ -74,54 +65,20 @@ extern "C" void curr_el_spx_sync(uint64_t esr, uint64_t elr, uint64_t spsr,
 	for (;;);
 }
 
-extern "C" void curr_el_spx_irq(uint64_t esr, uint64_t elr, uint64_t spsr,
-	uint64_t far)
-{
-	(void)esr;
-	(void)elr;
-	(void)spsr;
-	(void)far;
-
-	static constexpr uintptr_t perepherial_base = 0x3F00'0000;
-	static constexpr uintptr_t mbox_base = perepherial_base + 0x0000'B880;
-	static const volatile uint32_t* const mbox_read =
-		(uint32_t *)(mbox_base + 0x00);
-	static const volatile uint32_t * const mbox_status =
-		(uint32_t *)(mbox_base + 0x18);
-	static constexpr uint32_t mbox_status_empty = 1u << 30;
-
-	uint32_t pending = *arm_base_irq_pending;
-	printf("[IRQ] ARM IRQ PEND: 0x%08x%s\n", pending,
-		pending & 0x02 ? " (arm mailbox irq)" : "");
-
-	while (!(*mbox_status & mbox_status_empty)) {
-		uint32_t msg_addr = *mbox_read;
-		unsigned channel = msg_addr & 0xFu;
-		void *msg = reinterpret_cast<void *>(msg_addr & ~0xFu);
-
-		printf("[IRQ] mbox ch:%u@%p\n", channel, msg);
-	}
-}
-
 int main(void)
 {
-	uart::init();
+	arch::irq::init();
 	sandwich::sched::init();
+	uart::init();
 	bcm2837::mbox::init();
 
+	printf("cpu0 EL%u\n", get_current_el());
 	sandwich::sched::task_t task("task", sched_task);
 	task.wakeup();
 
-	printf("cpu0 EL%u\n", get_current_el());
-
-	printf("vectors@%p\n", vectors);
-	set_vbar_el1(vectors);
 	arch::irq::enable();
 
 	/* Set to enable ARM Mailbox IRQ. Write 1b set register */
-	*arm_base_irq_en = 1u << 1;
-	printf("ARM IRQ PEND: 0x%08x\n", *arm_base_irq_pending);
-
 #if 0
 	alignas(16) volatile char mbox_msg[] = {
 		0x20, 0x00, 0x00, 0x00, /* message size in bytes */
