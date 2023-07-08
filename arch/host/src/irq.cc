51b1917b5a5c478372cc1bd80f0971a9dba2dfe8
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2023-07-08 20:58:47 +0900

    Add host IRQ functions

--- arch/host/src/irq.cc
+++ arch/host/src/irq.cc
@@ -1,5 +1,20 @@
 #include <arch/irq.h>
 
+void arch::irq::init(void)
+{
+}
+
+void irq_en(unsigned irq_nr)
+{
+	(void)irq_nr;
+}
+
+void irq_en(unsigned irq_nr, void (*isr)(void))
+{
+	(void)irq_nr;
+	(void)isr;
+}
+
 arch::irq::flags_t arch::irq::disable(void)
 {
 	return 0;
